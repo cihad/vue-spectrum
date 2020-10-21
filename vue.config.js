1b52fcb2a7db7f6b0db727b494c0be0e258f2253
Cihad Paksoy <cihadp@gmail.com>
2020-10-21 14:03:21 +0300

    fix: publicPath

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,9 @@
 // const addVueRawPreLoader = require("../vue-raw-pre");
 
 module.exports = {
+	publicPath: process.env.NODE_ENV === 'production'
+		? '/vue-spectrum/'
+		: '/',
 	chainWebpack: (config) => {
 		// addVueRawPreLoader(config, {
 		// 	componentName: "example-demo",
